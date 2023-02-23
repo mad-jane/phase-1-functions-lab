60b28456a97ad69f1b9789cbfb571c9348ee71dd
madeline <madeline.j.dick@gmail.com>
2023-02-23 13:44:40 -0600

    Worked through lab with cohort

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(42 - blocks)
+}
+
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264
+}
+
+const distanceTravelledInFeet = (pickup, dropoff) => {
+    return Math.abs(pickup - dropoff) * 264
+}
+
+const calculatesFarePrice = (start, destination) => {
+    const feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400) {
+        return 0
+    } else if (feet > 400 && feet < 2000) {
+        return (feet - 400) * .02
+    } else if (feet >= 2000 && feet <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
